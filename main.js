1e2c5868b550c02afc98bf4d9eb603544ed249ad
tymmesyde <tymmesyde@users.noreply.github.com>
2025-12-10 11:37:46 +0000

    deploy: a36740cfc3fb7961f02ebfc83e6031e728b53e48

--- main.js
+++ main.js
@@ -8,6 +8,13 @@ const CUSTOM_NAMESPACE = 'urn:x-cast:com.stremio.cast';
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 
+const playbackConfig = (Object.assign(new cast.framework.PlaybackConfig(), playerManager.getPlaybackConfig()));
+playbackConfig.autoPauseDuration = 10;
+playbackConfig.autoResumeDuration = 3;
+playbackConfig.autoResumeNumberOfSegments = 1;
+playbackConfig.enableSmoothLiveRefresh = true;
+playerManager.setPlaybackConfig(playbackConfig);
+
 const castReceiverOptions = new cast.framework.CastReceiverOptions();
 castReceiverOptions.useShakaForHls = true;
 castReceiverOptions.customNamespaces = {
