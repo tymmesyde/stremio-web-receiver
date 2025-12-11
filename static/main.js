487a6eaf4cc6a5d9d6cfe567488eb13bf730206f
Tim <tymmesyde@gmail.com>
2025-12-11 19:49:25 +0100

    feat: add shakaConfig

--- static/main.js
+++ static/main.js
@@ -5,6 +5,14 @@ const ERROR_REASON = cast.framework.messages.ErrorReason;
 
 const playbackConfig = new cast.framework.PlaybackConfig();
 playbackConfig.autoResumeDuration = 5;
+playbackConfig.shakaConfig = {
+    // https://shaka-player-demo.appspot.com/docs/api/shaka.extern.html#.StreamingConfiguration
+    streaming: {
+        preferNativeHls: true,
+        bufferingGoal: 5,
+        bufferBehind: 5,
+    },
+};
 
 const options = new cast.framework.CastReceiverOptions();
 options.useShakaForHls = true;
