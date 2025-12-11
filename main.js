ef0882bd1e6d641477a2dc759f33a260611e838b
tymmesyde <tymmesyde@users.noreply.github.com>
2025-12-11 19:27:03 +0000

    deploy: f879d7913daf9b34b757564f95b5ad84727294eb

--- main.js
+++ main.js
@@ -5,12 +5,17 @@ const ERROR_REASON = cast.framework.messages.ErrorReason;
 
 const playbackConfig = new cast.framework.PlaybackConfig();
 playbackConfig.autoResumeDuration = 5;
+playbackConfig.enableUITextDisplayer = false;
 playbackConfig.shakaConfig = {
     // https://shaka-player-demo.appspot.com/docs/api/shaka.extern.html#.StreamingConfiguration
     streaming: {
         preferNativeHls: true,
         bufferingGoal: 5,
         bufferBehind: 5,
+        // https://shaka-player-demo.appspot.com/docs/api/shaka.extern.html#.RetryParameters
+        retryParameters: {
+            maxAttempts: 3,
+        },
     },
 };
 
